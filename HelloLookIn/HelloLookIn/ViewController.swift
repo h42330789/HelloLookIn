f189c74e264c0b33429a6d37028a38ea341c7260
MacBook Pro <aaa@bb.com>
2023-08-03 22:47:20 +0800

    init: init project

---
+++ HelloLookIn/HelloLookIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloLookIn
+//
+//  Created by MacBook Pro on 6/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloLookIn/Pods/Target Support Files/Pods-HelloLookIn-HelloLookInUITests/Pods-HelloLookIn-HelloLookInUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HelloLookIn_HelloLookInUITests : NSObject
+@end
+@implementation PodsDummy_Pods_HelloLookIn_HelloLookInUITests
+@end
